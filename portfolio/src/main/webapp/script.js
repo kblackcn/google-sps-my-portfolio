e275fa916d0ccb1c30aaa199e253160add2a813f
Yiran Zhou <zhouyiranhen2@outlook.com>
2020-07-16 19:36:50 +0800

    Update portfolio

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
-
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
-
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+(function() {
+    var current = 0;
+    document.onkeypress = function() {
+        current += 1;
+        target = "part_" + current;
+        item = document.getElementById(target);
+        if (item != null) {
+            item.setAttribute("style", "");
+        }
+     }
+})();
